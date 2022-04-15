2b76bef90127913e4d9e3cccd8b7ff52f2ec6ecf
HP <HP@192.168.56.1>
2022-04-15 16:35:32 +0000

    la seance 1

---
+++ gestion_note/src/gestion_note/Main.java
@@ -0,0 +1,10 @@
+package gestion_note;
+
+public class Main {
+
+	public static void main(String[] args) {
+		etudiant e1;
+		e1= new etudiant(56472 , "hsksk");
+	}
+
+}
--- gestion_note/src/gestion_note/etudiant.java
+++ gestion_note/src/gestion_note/etudiant.java
@@ -1,7 +1,5 @@
 package gestion_note;
 
 public class etudiant {
-	private int id;
-	private String nom;
-	private double note;
+
 }
